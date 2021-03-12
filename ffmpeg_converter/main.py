7995ebbc71ec1b1fdf25378e5a0679f0f42762cd
Alexander Ganuschak <a.ganushchak@visionlabs.ru>
2021-03-13 00:52:55 +0300

    start doing ffmpeg video converter

---
+++ ffmpeg_converter/main.py
@@ -0,0 +1,25 @@
+import argparse
+import subprocess
+
+
+def convert(args):
+
+    ffmpeg_args = ['ffmpeg']
+    ffmpeg_args += ['-i', args.input]
+    ffmpeg_args += ['-vf', f'scale={args.resolution}']
+    ffmpeg_args += ['-c:v', args.codec]
+    ffmpeg_args += [args.output]
+    proc = subprocess.Popen(args, shell=True)
+    proc.wait()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', dest='input', type=str, default='', required=True)
+    parser.add_argument('--output', dest='output', type=str, default='', required=False)
+    parser.add_argument('--resolution', dest='resolution', help='format width:height', type=str, default='', required=True)
+    parser.add_argument('--codec', dest='codec', type=str, default='', required=True)
+    args = parser.parse_args()
+    convert(args)
+
+
